d3400153a35dc87f42c6b220dd01552d8dddb53d
hangjianli <lihangjian123@ucla.edu>
2019-10-04 17:58:04 -0700

    implementing parameter class

---
+++ python/model.py
@@ -0,0 +1,14 @@
+import logging
+import random
+import numpy as np
+
+# Gets or creates a logger
+logger = logging.getLogger(__name__)
+
+# set log level and format
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+
+
+
+logger.info('Testing the project and environment.')
