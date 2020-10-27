9b146d038d10647355bf5169d118b6e3057828fe
hangjianli <lihangjian123@ucla.edu>
2020-10-27 14:12:32 -0400

    generate sim specs and true estimators

--- R/newalgo/sim.R
+++ R/newalgo/sim.R
@@ -94,6 +94,22 @@ diag(htheta)
 norm(htheta - estimands$theta, 'f')^2 / args$n^2
 
 # test bcd ----------------------------------------------------------------
+rm(list = ls())
+setwd("~/Documents/research/dag_network/")
+source("R/loadpackages.R")
+source("R/helper_funcs.R")
+source("R/gen_params_funcs.R")
+source("R/newalgo/functions.R")
+# generate specs ----------------------------------------------------------
+args <- args_for_parameter()  
+dir.create(path = paste0('output/', args$setting))
+saveRDS(args, file = paste0('output/', args$setting, "/args.rds"))
+estimands <- generate_parameters(args = args, seed = 1)
+image(estimands$theta)
+image(as(estimands$b, class(estimands$theta)))
+estimands$b[estimands$b!=0] %>% as.numeric() %>% hist(breaks=20)
+saveRDS(estimands, file = paste0('output/', args$setting, "/estimands.rds"))
+# run simulation ----------------------------------------------------------
 setwd("~/Documents/research/dag_network")
 sim_newalgo_ordered(args, estimands, start_sim=1, end_sim=args$num_sim, lamLen=15)
 process_output_ordered(simID='001')
