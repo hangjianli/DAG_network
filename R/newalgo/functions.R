c0f71490da02b52284523c1b7b750c2b7b8691cd
hangjianli <lihangjian123@ucla.edu>
2020-10-28 02:50:11 -0400

    added simulation code for unordered dag

--- R/newalgo/functions.R
+++ R/newalgo/functions.R
@@ -59,10 +59,15 @@ run_bcd <- function(
   }
   cat(paste0('[INFO] diff_beta: ', round(diff_beta, 7), "\n"))
   cat(paste0('[INFO] diff_theta: ', round(diff_theta, 7), "\n"))
+  dimnames(bhat) <- list(dimnames(X)[[2]], dimnames(X)[[2]])
   return(list(bhat=bhat, thetahat=thetahat))
 }
 
-get_sample_cov <- function(X, h_omega, h_beta){
+get_sample_cov <- function(
+  X,
+  h_omega, 
+  h_beta
+){
   n <- dim(X)[1]
   p <- dim(X)[2]
   rho_root <- 1 / h_omega
@@ -197,7 +202,6 @@ networkDAG_sol_path <- function(
       estimands = estimands, 
       maxIter = maxIter,
       tol = 1e-7)
-    
     # check if max degree s < n
     if(any(apply(res$bhat, 2, function(x) sum(abs(x) > 1e-4)) >= n)){
       cat("[INFO] lambda is too small, algorithm stops at k = ", k, "\n")
@@ -277,7 +281,13 @@ bench_sol_path <- function(
   }
 }
 
-sim_newalgo_ordered <- function(args, estimands, start_sim, end_sim, lamLen=10){
+sim_newalgo_ordered <- function(
+  args, 
+  estimands,
+  start_sim, 
+  end_sim,
+  lamLen=10
+){
   for(sim in start_sim:end_sim){
     dir.create(path = paste0("output/",args$setting, "/", args$setting, "--", sim))
     setwd(paste0("output/",args$setting, "/", args$setting, "--", sim))
@@ -290,15 +300,15 @@ sim_newalgo_ordered <- function(args, estimands, start_sim, end_sim, lamLen=10){
     saveRDS(X_, file = "X.rds")
     X <- X_$X
     networkDAG_sol_path(
-      X,
+      X = X, 
       block_size=args$block_size, 
-      estimands, 
+      estimands = estimands, 
       lambda_len = lamLen
     )
     bench_sol_path(
-      X,
+      X = X,
       block_size=args$block_size, 
-      estimands, 
+      estimands = estimands, 
       lambda_len = lamLen
     )
     setwd("~/Documents/research/dag_network")
@@ -314,28 +324,28 @@ get_shd_ordered <- function(
   sim,
   simID,
   bstar_adj, 
-  estimands, 
+  s0, 
   thresh = 0.1
 ){
-  s0 <- estimands$s0
   output_ordered <- vector(mode = "list")
   # main --------------------------------------------------------------------
-  #BIC
+  # BIC
   main_best_bic <- readRDS(file = paste0(simID, '--', sim, '/main_lam_', kmainbic, '.rds'))
   bhat_adj <- 1*(abs(main_best_bic$bhat) > thresh)
   shdXmain <- compute_SHD_dag(adj1 = bhat_adj, adj_true = bstar_adj, s0) %>% unlist()
   output_ordered$shdXmain=shdXmain
-  #minCor
+  # minCor
   main_best_cor <- readRDS(file = paste0(simID, '--', sim, '/main_lam_', kmaincor, '.rds'))
   bhat_adj <- 1*(abs(main_best_cor$bhat) > thresh)
   shdXmainCor <- compute_SHD_dag(adj1 = bhat_adj, adj_true = bstar_adj, s0) %>% unlist()
   output_ordered$shdXmainCor = shdXmainCor
   # baseline ----------------------------------------------------------------
+  # BIC
   baseline_best_bic <- readRDS(file = paste0(simID, '--', sim, '/baseline_lam_', kbenchbic, '.rds'))
   bhat_adj <- 1*(abs(baseline_best_bic$bhat) > thresh)
   shdXbaseline <- compute_SHD_dag(adj1 = bhat_adj, adj_true = bstar_adj, s0) %>% unlist()
   output_ordered$shdXbaseline=shdXbaseline
-  #minCor
+  # minCor
   baseline_best_cor <- readRDS(file = paste0(simID, '--', sim, '/baseline_lam_', kbenchcor, '.rds'))
   bhat_adj <- 1*(abs(baseline_best_cor$bhat) > thresh)
   shdXbaselineCor <- compute_SHD_dag(adj1 = bhat_adj, adj_true = bstar_adj, s0) %>% unlist()
@@ -344,7 +354,10 @@ get_shd_ordered <- function(
 }
 
 
-process_output_ordered <- function(simID='001', thr=0.1){
+process_output_ordered <- function(
+  simID='001', 
+  thr=0.1
+){
   setwd(paste0('output/', simID))
   args <- readRDS('args.rds')
   estimands <- readRDS('estimands.rds')
@@ -354,8 +367,6 @@ process_output_ordered <- function(simID='001', thr=0.1){
     minrowcor_main <- readRDS(paste0(simID, '--', sim, '/minrowcor_main.rds'))  
     BICscores_baseline <- readRDS(paste0(simID, '--', sim, '/BICscores_baseline.rds'))  
     minrowcor_baseline <- readRDS(paste0(simID, '--', sim, '/minrowcor_baseline.rds'))  
-    X_ <- readRDS(paste0(simID, '--', sim, '/X.rds'))  
-    X <- X_$X
     bestk_bic_main <- which.min(BICscores_main)
     bestk_cor_main <- which.min(minrowcor_main)
     bestk_bic_baseline <- which.min(BICscores_baseline)
@@ -365,18 +376,23 @@ process_output_ordered <- function(simID='001', thr=0.1){
       kmaincor = bestk_cor_main, 
       kbenchbic = bestk_bic_baseline, 
       kbenchcor = bestk_cor_baseline,
-      sim,
-      simID,
-      bstar_adj, 
-      estimands, 
-      thresh = thr)
+      sim = sim,
+      simID = simID,
+      bstar_adj = bstar_adj, 
+      s0 = estimands$s0, 
+      thresh = thr
+    )
     saveRDS(SHD_stats, file = paste0(simID, '--', sim, "/SHDstats.rds"))
   }
   setwd("~/Documents/research/dag_network")
 }
 
 
-get_all_shd_ordered <- function(simID, estimands, num_sim){
+get_all_shd_ordered <- function(
+  simID, 
+  estimands,
+  num_sim
+){
   thrs <- seq(0, 0.5,length.out = 10)
   allShdS <- vector(mode = "list", length = length(thrs))
   bstar_adj <- 1*(abs(estimands$b) > 0)
@@ -397,11 +413,12 @@ get_all_shd_ordered <- function(simID, estimands, num_sim){
         kmaincor = bestk_cor_main, 
         kbenchbic = bestk_bic_baseline, 
         kbenchcor = bestk_cor_baseline,
-        sim,
+        sim = sim,
         simID = simID,
         bstar_adj, 
         estimands, 
-        thresh = thrs[j])
+        thresh = thrs[j]
+      )
     }
     bestbench <- which.min(sapply(
       allShdS, 
@@ -415,7 +432,16 @@ get_all_shd_ordered <- function(simID, estimands, num_sim){
   }
 }
 
-get_average_shd <- function(simID, nsim){
+get_all_shd_unordered <- function(
+  simID, 
+  estimands,
+  num_sim
+){
+  return()
+}
+
+
+get_average_shd_ordered <- function(simID, nsim){
   SHDres <- readRDS(paste0("output/", simID, '/', simID, '--1/', "SHDclose.rds"))
   num_statistic <- length(names(SHDres[[1]]))
   total <- data.frame(
@@ -436,3 +462,180 @@ get_average_shd <- function(simID, nsim){
   total$B0 = c(estimands$s0, rep(0,num_statistic-1))
   saveRDS(total, paste0("output/", simID, "/shd_average.rds"))
 }
+
+get_average_shd_unordered <- function(simID, nsim){
+  SHDres <- readRDS(paste0("output/", simID, '/', simID, '--1/', "SHDstats.rds"))
+  num_statistic <- length(names(SHDres[[1]]))
+  total <- data.frame(
+    row.names = names(SHDres[[1]]), 
+    shdXmain = rep(0, num_statistic),
+    shd_pc = rep(0, num_statistic),
+    shd_pc_decor=rep(0, num_statistic),
+    shd_ges=rep(0, num_statistic),
+    shd_ges_decor=rep(0, num_statistic)
+  )
+  for (sim in 1:nsim){
+    allshd <- as.data.frame(
+      readRDS(paste0("output/", simID,  '/', simID, "--", sim, "/SHDstats.rds"))
+    )
+    allshd <- allshd[, names(total)]
+    total <- total + allshd
+  }
+  total <- round(total / nsim, 2)
+  total$B0 = c(estimands$s0, rep(0,num_statistic-1))
+  saveRDS(total, paste0("output/", simID, "/shd_average.rds"))
+}
+
+
+GES_sol <- function(X, decor=F){
+  n <- dim(X)[1]
+  p <- dim(X)[2]
+  set.seed(100)
+  fgsX <- tetradrunner(
+    algoId = 'fges',
+    df = X,
+    scoreId = 'sem-bic',
+    dataType = 'continuous',
+    faithfulnessAssumed=TRUE,
+    maxDegree=-1,
+    verbose=TRUE
+  )
+  adjmat_fgesCPDAG_X <- get_adjmat_from_fges(
+    fgsX$edges,
+    p = p, 
+    varnames = dimnames(X)[[2]]
+  )
+  if(decor){
+    saveRDS(adjmat_fgesCPDAG_X, file = 'adjmat_fges_CPDAG_decor.rds')  
+  }else{
+    saveRDS(adjmat_fgesCPDAG_X, file = 'adjmat_fges_CPDAG.rds')  
+  }
+}
+
+pc_sol <- function(X, decor=F){
+  n <- dim(X)[1]
+  p <- dim(X)[2]
+  suffstat <- list(C = cor(X), n = n)
+  res_pc <- pcalg::pc(
+    suffStat = suffstat, 
+    indepTest = gaussCItest, 
+    alpha = 0.05, 
+    labels = dimnames(X)[[2]]
+  )
+  adjmat_pc_CPDAG <- as(res_pc, "amat")
+  if(decor){
+    saveRDS(adjmat_pc_CPDAG, file = 'adjmat_pc_CPDAG_decor.rds')
+  }
+  else{
+    saveRDS(adjmat_pc_CPDAG, file = 'adjmat_pc_CPDAG.rds')  
+  }
+}
+
+get_Xdecor <- function(Xp){
+  bic_score <- readRDS('BICscores_main.rds')
+  best_bic <- which.min(bic_score)
+  best_res <- readRDS(paste0('main_lam_', best_bic, '.rds'))
+  X_decor <- chol(best_res$thetahat) %*% Xp
+  dimnames(X_decor) <- dimnames(Xp)
+  return(X_decor)
+}
+
+
+sim_newalgo_unordered <- function(
+  args, 
+  estimands, 
+  start_sim=1, 
+  end_sim=args$num_sim, 
+  lamLen=15){
+  for(sim in start_sim:end_sim){
+    dir.create(path = paste0("output/",args$setting, "/", args$setting, "--", sim))
+    setwd(paste0("output/",args$setting, "/", args$setting, "--", sim))
+    X_ <- sim_X(vers = sim, 
+                n = args$n,
+                p = estimands$realp,
+                omg.sq = estimands$omg.sq,
+                sig = estimands$sig, 
+                b = estimands$b)
+    X <- X_$X
+    XXp <- Permute_X(X, seed = sim)
+    Xp <- XXp$Xperm
+    saveRDS(X_, file = "X.rds")
+    saveRDS(XXp, file = "Xp.rds")
+    # Other methods -----------------------------------------------------------
+    GES_sol(Xp, decor = F)
+    pc_sol(Xp, decor = F)
+    # NetworkDAG --------------------------------------------------------------
+    networkDAG_sol_path(
+      X = Xp, 
+      block_size=args$block_size, 
+      estimands = estimands, 
+      lambda_len = lamLen,
+      maxIter = 100
+    )
+    # decorrelation -----------------------------------------------------------
+    Xdecor <- get_Xdecor(Xp)
+    GES_sol(Xdecor, decor = T)
+    pc_sol(Xdecor, decor = T)
+    setwd("~/Documents/research/dag_network")  
+  }
+}
+
+
+get_shd_unordered <- function(
+  bestk_bic_main, 
+  sim,
+  simID,
+  bstar_adj, 
+  s0, 
+  thresh = 0.1
+){
+  output_unordered <- vector(mode = "list")
+  main_best_bic <- readRDS(file = paste0(simID, '--', sim, '/main_lam_', bestk_bic_main, '.rds'))
+  bhat_adj_cpdag <- bnstruct::dag.to.cpdag(1*(abs(main_best_bic$bhat) > thresh))
+  shdXmain <- unlist(compute_SHD_detail(bhat_adj_cpdag, bstar_adj, s0))
+  # get PC shd --------------------------------------------------------------
+  adjmat_pc_CPDAG <- readRDS(
+    file = paste0(simID, '--', sim, '/adjmat_pc_CPDAG.rds'))
+  adjmat_fges_CPDAG_decor <- readRDS(
+    file = paste0(simID, '--', sim, '/adjmat_pc_CPDAG_decor.rds'))
+  shd_pc <- unlist(compute_SHD_detail(adjmat_pc_CPDAG, bstar_adj, s0))
+  shd_pc_decor <- unlist(compute_SHD_detail(adjmat_fges_CPDAG_decor, bstar_adj, s0))
+  # get GES shd -------------------------------------------------------------
+  adjmat_fges_CPDAG <- readRDS(
+    file = paste0(simID, '--', sim, '/adjmat_fges_CPDAG.rds'))
+  adjmat_fges_CPDAG_decor <- readRDS(
+    file = paste0(simID, '--', sim, '/adjmat_fges_CPDAG_decor.rds'))
+  shd_ges <- unlist(compute_SHD_detail(adjmat_fges_CPDAG, bstar_adj, s0))
+  shd_ges_decor <- unlist(compute_SHD_detail(adjmat_fges_CPDAG_decor, bstar_adj, s0))
+  output_unordered$shdXmain=shdXmain
+  output_unordered$shd_pc=shd_pc
+  output_unordered$shd_pc_decor=shd_pc_decor
+  output_unordered$shd_ges=shd_ges
+  output_unordered$shd_ges_decor=shd_ges_decor
+  return(output_unordered)
+}
+
+
+process_output_unordered <- function(simID = simID, thr = 0.1){
+  setwd(paste0('output/', simID))
+  args <- readRDS('args.rds')
+  estimands <- readRDS('estimands.rds')
+  bstar_adj_cpdag <- bnstruct::dag.to.cpdag(1*(estimands$b != 0)) 
+  for(sim in 1:args$num_sim){
+    cat(paste0('[INFO] Processing sim ', sim, '\n'))
+    BICscores_main <- readRDS(paste0(simID, '--', sim, '/BICscores_main.rds'))  
+    bestk_bic_main <- which.min(BICscores_main)
+    SHD_stats <- get_shd_unordered(
+      bestk_bic_main = bestk_bic_main, 
+      sim = sim,
+      simID = simID,
+      bstar_adj = bstar_adj_cpdag, 
+      s0 = estimands$s0, 
+      thresh = thr
+    )
+    saveRDS(SHD_stats, file = paste0(simID, '--', sim, "/SHDstats.rds"))
+  }
+  setwd("~/Documents/research/dag_network")
+}
+
+
