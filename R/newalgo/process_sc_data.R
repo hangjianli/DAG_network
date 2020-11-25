095c2e0cbf01c10159ada1a3a7085fcb3a900e83
hangjianli <lihangjian123@ucla.edu>
2020-11-24 23:34:52 -0500

    added some functions for single cell data experiments

--- R/newalgo/process_sc_data.R
+++ R/newalgo/process_sc_data.R
@@ -1,47 +1,31 @@
-
-
-
 # real data ---------------------------------------------------------------
-
-single_cell <- read_csv("data/GSE75748_sc_cell_imputationscimpute_count.csv")
-single_cell %>% dim()
-names(single_cell)[1] <- 'geneID'
+library(readr)
+# single_cell <- read_csv("data/single_cell_data/GSE75748_sc_cell_imputationscimpute_count.csv")
+# single_cell %>% dim()
+# names(single_cell)[1] <- 'geneID'
 # saveRDS(single_cell, "data/single_cell.rds")
-single_cell[1:5,1:5]
-
-library(readxl)
-gene_names <- read_excel("data/single_cell.xlsx")
-gene_names
-
-"EOMES" %in% single_cell$geneID
 
 # 
-sig_genes <- single_cell %>% filter(geneID %in% gene_names$GeneID)
-
+single_cell <- readRDS("data/single_cell_data/single_cell.rds")
+sig_genes <- single_cell 
+# %>% filter(geneID %in% gene_names$GeneID)
 sig_genes %>% head()
-
-test <- substr(names(sig_genes), start = 0, stop = 3)
-sub(pattern = "_$", "", x = test)[-1] %>% table() 
+# test <- substr(names(sig_genes), start = 0, stop = 3)
+# sub(pattern = "_$", "", x = test)[-1] %>% table() 
 sig_genes[, -1] <- log10(sig_genes[, -1] + 1.01)
-
-
-sig_genes_log_val <- sig_genes_log[, -1] %>% as.matrix()
-
+sig_genes_log_val <- sig_genes[, -1] %>% as.matrix()
 dimnames(sig_genes_log_val)[[1]] <- sig_genes$geneID
+saveRDS(sig_genes_log_val, "data/sig_genes_log_val_full.rds")
 
-saveRDS(sig_genes_log_val, "data/sig_genes_log_val.rds")
+# sig_genes_log_val %>% dim()
+library(dplyr)
 
+# find set of valid genes by coefficient of variation
 
+idx_goodgene <- apply(sig_genes_log_val, 1, function(x) sd(x)/abs(mean(x)) > 0.2) %>% which()
 
-
-Xp <- readRDS(file = "data/sig_genes_log_val.rds")
-dir.create(path = "output/single_cell")
-setwd(dir = "output/single_cell")
-
-networkDAG_sol_path(
-  X = Xp, 
-  block_size=args$block_size, 
-  estimands = estimands, 
-  lambda_len = lamLen,
-  maxIter = 100
-)
+goodgene <- sig_genes_log_val[idx_goodgene,]
+goodgene_3k <- sample_n(as.data.frame(goodgene), 3000) %>% as.matrix()
+goodgene_3k %>% str()
+# hierarchical clustering here
+library(hclust)
--- R/newalgo/sim.R
+++ R/newalgo/sim.R
@@ -5,6 +5,8 @@ source("R/loadpackages.R")
 source("R/helper_funcs.R")
 source("R/gen_params_funcs.R")
 source("R/newalgo/functions.R")
+
+
 # generate specs ----------------------------------------------------------
 args <- args_for_parameter()  
 dir.create(path = paste0('output/', args$setting))
@@ -18,8 +20,8 @@ saveRDS(estimands, file = paste0('output/', args$setting, "/estimands.rds"))
 # ordered 
 setwd("~/Documents/research/dag_network")
 simID <- args$setting
-sim_newalgo_ordered(args, estimands, start_sim=1, end_sim=args$num_sim, lamLen=15)
-process_output_ordered(simID = simID, thr = 0.1)
+sim_newalgo_ordered(args, estimands, start_sim=1, end_sim=args$num_sim, lamLen=10)
+process_output_ordered(simID = simID, estimands = estimands, thr = 0.1)
 get_all_shd_ordered(simID = simID, estimands, args$num_sim)
 get_average_shd_ordered(simID = simID, nsim = as.numeric(args$num_sim))
 
@@ -29,3 +31,85 @@ simID <- args$setting
 sim_newalgo_unordered(args, estimands, start_sim=1, end_sim=10, lamLen=10)
 process_output_unordered(simID = simID, thr = 0.1, nsim = args$num_sim)
 get_average_shd_unordered(simID = simID, nsim = as.numeric(args$num_sim))
+
+# single cell data --------------------------------------------------------
+
+
+
+setwd("~/Documents/research/dag_network/")
+sc_block_idx_full <- readRDS("data/single_cell_data/single_cell_block_idx_full.rds")
+Xp <- readRDS(file = "data/single_cell_data/sig_genes_log_val.rds")
+dir.create(path = "output/single_cell5")
+setwd(dir = "output/single_cell5")
+Xp <- t(Xp)
+Xp %>% dim()
+set.seed(2)
+# randomly sample 20 cells from each cell type and merge the indices
+res <- sample_sc_data(
+  full_log_vals = Xp, 
+  full_idx = sc_block_idx_full,
+  size = c(20,20,20,10,15,15,5),
+  seed = 4
+)
+saveRDS(res, "sc_subsampled.rds")
+block_idx <- get_cell_block_idx(cellnames = res$cellnames)
+networkDAG_sol_path(
+  X = res$subsetXp, 
+  block_size=20, 
+  zeropos_list = NULL,
+  block_idx = block_idx,
+  lambda_len = 10,
+  maxIter = 100
+)
+Xdecor_res <- get_Xdecor(res$subsetXp)
+
+GES_sol(res$subsetXp, decor = F)
+GES_sol(Xdecor_res$X_decor, decor = T)
+# GES_sol(Xdecor_res$X_decor_1iter, decor = T)
+pc_sol(Xdecor$X_decor, decor = T)
+pc_sol(res$subsetXp, decor = F)
+sparsebn_sol(Xdecor$X_decor, decor = T)
+sparsebn_sol(res$subsetXp, decor = F)
+fgesdag <- readRDS("adjmat_fges_CPDAG_decor.rds")
+fgesdag_original <- readRDS("adjmat_fges_CPDAG.rds")
+pcdag <- readRDS("adjmat_pc_CPDAG_decor.rds")
+pcdag_original <- readRDS("adjmat_pc_CPDAG.rds")
+sbndag <- readRDS("adjmat_sparsebn_CPDAG_decor.rds")
+sbndag_original <- readRDS("adjmat_sparsebn_CPDAG.rds")
+
+plot_cpdag(fgesdag_original)
+plot_cpdag(fgesdag)
+plot_cpdag(pcdag_original)
+plot_cpdag(pcdag)
+plot_cpdag(sbndag_original)
+plot_cpdag(sbndag)
+
+
+
+bic_score <- readRDS('BICscores_main.rds')
+best_res <- readRDS(paste0('main_lam_', best_bic, '.rds'))
+bstar_adj_cpdag <- bnstruct::dag.to.cpdag(1*(best_res$bhat != 0)) 
+which(best_res$bhat != 0, arr.ind = T)
+plot_cpdag(best_res$bhat)
+plot_cpdag(best_res$bhat_1iter)
+
+
+which(best_res$bhat[, 'POU5F1'] != 0)
+
+# 
+# X: num cells x num genes
+# X = [X1, X2]
+# 
+# X2 -> theta_hat
+# X1 -> cross validation 
+# 
+# likelihood1: CV(X1) without decor using PC, GES, ... theta_hat
+# likelihood2: CV(X1) with decor using PC, GES, ... theta_hat
+# 
+# in first case:
+#   X1 = X11 -> B_hat1,  X12 + B_hat1 + theta_hat -> likelihood1
+# 
+# in second case:
+#   X1 = decor(X11) -> B_hat2,  X12 + B_hat2 + theta_hat -> likelihood2
+# 
+# 
